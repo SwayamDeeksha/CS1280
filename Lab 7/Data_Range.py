1ab34a45e5ecf29ea7e6a0633c68c5b212a7d79b
Swayam Deeksha <swadeeku.007@gmail.com>
2025-03-07 10:30:45 +0530

    Update Data_Range.py

--- Lab 7/Data_Range.py
+++ Lab 7/Data_Range.py
@@ -1,3 +1,4 @@
+import pandas as pd
 # Create a DataFrame from a date_range()
 dates = pd.date_range("20130101", periods=6)
 df_2 = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list("ABCD"))
