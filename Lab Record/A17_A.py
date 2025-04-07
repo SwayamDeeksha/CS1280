d2f2b39cc8f23aabb1d9e4bd7f13a0e7eb9bfc8d
Swayam Deeksha <swadeeku.007@gmail.com>
2025-04-07 10:32:22 +0530

    Activities

--- Lab Record/A17_A.py
+++ Lab Record/A17_A.py
@@ -3,6 +3,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('5ds_salaries.csv')
-sns.barplot(x='categorical_column', y='numerical_column', data=df)
+sns.barplot(x = 'salary_currency', y='salary_in_usd', data=df)
 plt.xticks(rotation=45)
 plt.show()
